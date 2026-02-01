0a47996c3afd76ed90188974b8cccd5753457839
AnnaZ0203 <annazhoga0203@gmail.com>
2026-02-01 18:42:08 +0000

    first commit

---
+++ dist/mini_shop6.538759f8.js
@@ -0,0 +1,2 @@
+document.getElementById("btn"),btn.addEventListener("click",()=>{let e=document.getElementById("a").value,t=document.getElementById("b").value;document.getElementById("result").textContent="Result"+(Number(e)+t)});
+//# sourceMappingURL=mini_shop6.538759f8.js.map
